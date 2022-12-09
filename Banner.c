5444ff9838e4ef8c0dff2e7a291cf9e8eba1ff24
septyanagstn <117805786+septyanagstn@users.noreply.github.com>
2022-12-09 16:19:33 +0700

    Add files via upload

---
+++ Banner.c
@@ -0,0 +1,17 @@
+/*	DESAIN MAIN MENU TIC TAC TOE
+  	Property of Tritoll
+  	Bob Manuel
+  	Septyana Agustina
+  	Zahran Anugerah Rizqullah		*/
+
+#include<stdio.h>
+
+int main(){
+	printf("\n\t\t ___________  ___  _______    ___________  ________  _______   ___________  ________  ________ ");
+	printf("\n\t\t/___   ____/ /  / /  ____/   /___   ____/ /  __   / /  ____/  /___   ____/ /  __   / /  _____/ ");
+	printf("\n\t\t   /  /     /  / /  /           /  /     /  /_/  / /  /          /  /     /  / /  / /  /___    ");
+	printf("\n\t\t  /  /     /  / /  /           /  /     /  __   / /  /          /  /     /  / /  / /  ____/    ");
+	printf("\n\t\t /  /     /  / /  /___        /  /     /  / /  / /  /___       /  /     /  /_/  / /  /____     ");
+	printf("\n\t\t/__/     /__/ /______/       /__/     /__/ /__/ /______/      /__/     /_______/ /_______/     \n");
+	return 0;
+}
